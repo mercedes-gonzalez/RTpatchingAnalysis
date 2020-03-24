d532a22a414561f8d56a7a94b630190c60259569
mgonzalez91 <m.gonzalez@gatech.edu>
2020-03-23 21:13:55 -0400

    pyabf addition!

--- analyzeFile.py
+++ analyzeFile.py
@@ -3,8 +3,12 @@ This script reads data from data files
 and counts APs. For now. 
 Mercedes Gonzalez. March 2020. 
 '''
-import numpy
-from countAPs import countAPs
+import numpy as np
+import scipy as sp
+import pyabf
+import matplotlib as plt
+import axographio as axo
+from patchAnalysis import * 
 
 print('counting APs...')
 countAPs()
--- patchAnalysis.py
+++ patchAnalysis.py
@@ -1,4 +1,18 @@
+import numpy as np
+import scipy as sp
+import pyabf
+import matplotlib.pyplot as plt
+import axographio as axo
+
 def countAPs():
-    num_APs = 10
-    print(str(num_APs))
+    abf = pyabf.ABF('C:/Users/mgonzalez91/Dropbox (GaTech)/Research/All Things Emory !/Emory-Patching/1-23-2020/2020_01_23_0016.abf')
+    print(abf,'\n')
+    # abf.headerLaunch()
+
+    # Access sweep data
+    for i in range(3):
+        abf.setSweep(i)
+        plt.plot(abf.sweepX,abf.sweepY, alpha=.5, label="sweep %d" % (i))
+    plt.legend()
+    plt.show()
     return
