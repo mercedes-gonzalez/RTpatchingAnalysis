64f714ed7a18e79c28545cba85d292e1d1622167
mgonzalez91 <m.gonzalez@gatech.edu>
2020-03-26 18:22:22 -0400

    fire frequency vs current density plot, csv create

--- analyzeFile.py
+++ analyzeFile.py
@@ -3,6 +3,10 @@ This script reads data from data files
 and counts APs. For now. 
 Mercedes Gonzalez. March 2020. 
 '''
+# Import necessary libraries
+from os import listdir
+from os.path import isfile, join
+import csv
 import numpy as np
 import scipy as sp
 import pyabf
@@ -10,5 +14,55 @@ import matplotlib as plt
 import axographio as axo
 from patchAnalysis import * 
 
-print('counting APs...')
-countAPs()
+# Init variables
+SHOW_PLOTS = False # Refers to individual sweep plots
+SHOW_FINAL_PLOT = False
+SHOW_HEAD = False
+my_path = "C:/Users/mgonzalez91/Dropbox (GaTech)/Research/All Things Emory !/Emory-Patching/1-23-2020/"
+csv_name = "Jan-23-Analysis.csv"
+csv_file = my_path + csv_name
+cap = 26.1
+
+# Read only abf files from directory 
+abf_files = [f for f in listdir(my_path) if isfile(join(my_path, f)) & f.endswith(".abf")]
+abf = pyabf.ABF((my_path+abf_files[0]))
+if SHOW_HEAD: abf.headerLaunch()
+
+# Analyze each file
+for file_name in abf_files:
+    full_file = my_path + file_name
+    abf = pyabf.ABF(full_file)
+
+    input_cmd = np.empty((abf.sweepCount,1))
+    AP_count = np.empty((abf.sweepCount,1))
+    input_dur = np.empty((abf.sweepCount,1))
+    
+    # Plot membrane test if available
+    # plotMembraneTest(abf)
+
+    # Analyze traces
+    for sweep_idx in range(abf.sweepCount):
+        input_cmd[sweep_idx], AP_count[sweep_idx], input_dur[sweep_idx] = countAPs(abf,sweep_idx,SHOW_PLOTS) # x is time, y is data, c is command
+        # print("input: %f, APs: %i\n" % (input_cmd[sweep_idx], AP_count[sweep_idx]))
+    
+    # Plot 
+    if SHOW_FINAL_PLOT:
+        plt.figure()
+        plt.plot(input_cmd,AP_count,"o")
+
+if SHOW_FINAL_PLOT:
+    plt.xlabel("Input Current (pA)")
+    plt.ylabel("Num Action Potentials")
+    plt.show() 
+
+step_time = input_dur[0]/abf.dataRate*1000 # convert from input duration in samples to input duration in seconds
+step_time = .2 # temp 
+plt.figure()
+plt.plot(input_cmd/cap,AP_count/step_time)
+plt.plot(input_cmd/cap,AP_count/step_time,"o")
+plt.xlabel("Current Density (pA/pF)")
+plt.ylabel("Firing Frequency (Hz)")
+plt.show()
+# Save as csv
+all_data = np.concatenate((input_cmd,AP_count),axis=1)
+np.savetxt(csv_file, all_data, delimiter=',', fmt=['%f','%d'], header='Current (pA), Num APs')
--- patchAnalysis.py
+++ patchAnalysis.py
@@ -1,18 +1,91 @@
 import numpy as np
 import scipy as sp
+import scipy.signal as sig
 import pyabf
+import pyabf.tools.memtest as mem
 import matplotlib.pyplot as plt
 import axographio as axo
 
-def countAPs():
-    abf = pyabf.ABF('C:/Users/mgonzalez91/Dropbox (GaTech)/Research/All Things Emory !/Emory-Patching/1-23-2020/2020_01_23_0016.abf')
-    print(abf,'\n')
-    # abf.headerLaunch()
-
     # Access sweep data
-    for i in range(3):
-        abf.setSweep(i)
-        plt.plot(abf.sweepX,abf.sweepY, alpha=.5, label="sweep %d" % (i))
-    plt.legend()
+    # for i in range(3):
+    #     abf.setSweep(i)
+    #     plt.plot(abf.sweepX,abf.sweepY, alpha=.5, label="sweep %d" % (i))
+    # plt.legend()
+    # plt.show()
+
+def countAPs(abf, sweep_idx, show_plots):
+    # Purpose: To count the number of APs in a trace
+    # Inputs: N/A
+    # Outputs: N/A
+    abf.setSweep(sweep_idx, channel=0)
+    time = abf.sweepX
+    data = abf.sweepY
+
+    abf.setSweep(sweep_idx, channel=1)
+    command = abf.sweepY
+
+    # GET COMMAND
+    baseline_cmd = np.array(command[0:10]).mean() #get baseline command (no input)
+    is_on = command > baseline_cmd # Create logical mask for when command input is on
+    input_dur = np.sum(is_on) # number of samples collected with input ON
+    input_cmd = np.array(command[is_on]).mean() # get average of input command
+#     print("%f pA\n" % input_cmd) 
+
+
+    # GET NUMBER OF ACTION POTENTIALS
+    baseline_data = np.array(data[0:10]).mean() # get baseline data
+    peaks, _ = sig.find_peaks(data,height=.5*baseline_data,distance=50,rel_height=0)
+    num_APs = len(peaks)
+
+    if show_plots:
+        plt.figure(1)
+        plt.plot(time,data)
+        plt.plot(time[peaks], data[peaks],"x")
+        plt.show()
+#     print("%i APs\n" % num_APs)
+
+    return input_cmd, num_APs, input_dur
+
+def plotMembraneTest(abf):
+    # Purpose: To extract the membrane test parameters from an ABF file
+    # Inputs: N/A
+    # Outputs: N/A
+    memtest = mem.Memtest(abf)
+    fig = plt.figure(figsize=(8, 5))
+
+    ax1 = fig.add_subplot(221)
+    ax1.grid(alpha=.2)
+    ax1.plot(abf.sweepTimesMin, memtest.Ih.values,
+            ".", color='C0', alpha=.7, mew=0)
+    ax1.set_title(memtest.Ih.name)
+    ax1.set_ylabel(memtest.Ih.units)
+
+    ax2 = fig.add_subplot(222)
+    ax2.grid(alpha=.2)
+    ax2.plot(abf.sweepTimesMin, memtest.Rm.values,
+            ".", color='C3', alpha=.7, mew=0)
+    ax2.set_title(memtest.Rm.name)
+    ax2.set_ylabel(memtest.Rm.units)
+
+    ax3 = fig.add_subplot(223)
+    ax3.grid(alpha=.2)
+    ax3.plot(abf.sweepTimesMin, memtest.Ra.values,
+            ".", color='C1', alpha=.7, mew=0)
+    ax3.set_title(memtest.Ra.name)
+    ax3.set_ylabel(memtest.Ra.units)
+
+    ax4 = fig.add_subplot(224)
+    ax4.grid(alpha=.2)
+    ax4.plot(abf.sweepTimesMin, memtest.CmStep.values,
+            ".", color='C2', alpha=.7, mew=0)
+    ax4.set_title(memtest.CmStep.name)
+    ax4.set_ylabel(memtest.CmStep.units)
+
+    for ax in [ax1, ax2, ax3, ax4]:
+        ax.margins(0, .9)
+        ax.set_xlabel("Experiment Time (minutes)")
+        for tagTime in abf.tagTimesMin:
+            ax.axvline(tagTime, color='k', ls='--')
+    plt.tight_layout()
     plt.show()
     return
